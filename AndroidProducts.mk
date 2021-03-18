7d6524a6b83ab780206e4e033af2499b1533c509
Puneet Gautam <punitrajgautam@uiit.ac.in>
2021-03-18 19:29:07 +0530

    X01AD:Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_X01AD.mk
+    $(LOCAL_DIR)/cherish_X01AD.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_X01AD-user \
-    aosp_X01AD-userdebug \
-    aosp_X01AD-eng
+    cherish_X01AD-user \
+    cherish_X01AD-userdebug \
+    cherish_X01AD-eng
